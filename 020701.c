e9d292332fcffa2a963a28498824bf39e15fb817
xhChen <78328055+CHxh2001@users.noreply.github.com>
2021-02-07 21:30:18 +0800

    Add files via upload

---
+++ 020701.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (0 == a % 2)
+//		printf("该数是偶数。\n");
+//	else
+//		printf("该数是奇数。\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int i ;
+//	for (i = 1;i <= 100; i++)
+//	{
+//		if (1 == i % 2)
+//			printf("%d\n", i);
+//		else
+//			continue;
+//
+//	}
+//	return 0;
+//}
+int main()
+//{
+//	int day;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	//case 1:printf("星期1"); break;
+//	//case 2:printf("星期2"); break;
+//	//case 3:printf("星期3"); break;
+//	//case 4:printf("星期4"); break;
+//	//case 5:printf("星期5"); break;
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:printf("休息日"); break;
+//	case 6:printf("星期6"); break;
+//	case 7:printf("星期7"); break;
+//	default:printf("输入错误。\n"); break;//输入大于等于8;
+//
+//
+//	}
+//	return 0;
+//}
+{
+	int ch = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		putchar(ch);
+	}
+	return 0;
+}
